a27263452522074bc6954afa5a2f066b6b59f6c7
Jaime López <114154176+Jaime1999l@users.noreply.github.com>
2024-11-09 17:52:12 +0100

    Update AuthenticationManager.java

--- app/src/main/java/com/myproyect/HistorialMedico/medicalHistory/AuthenticationManager.java
+++ app/src/main/java/com/myproyect/HistorialMedico/medicalHistory/AuthenticationManager.java
@@ -1,7 +1,25 @@
 package com.myproyect.HistorialMedico.medicalHistory;
 
+import android.content.Context;
+
+import DB.DatabaseHelper;
+import DB.User;
+
 public class AuthenticationManager {
-    public static boolean authenticate(String username, String password) {
-        return "user".equals(username) && "password".equals(password);
+
+    public interface AuthenticationCallback {
+        void onAuthenticationSuccess(User user);
+        void onAuthenticationFailure();
+    }
+
+    public static void authenticate(Context context, String username, String password, AuthenticationCallback callback) {
+        new Thread(() -> {
+            User user = DatabaseHelper.getDatabase(context).userDao().authenticate(username, password);
+            if (user != null) {
+                callback.onAuthenticationSuccess(user);
+            } else {
+                callback.onAuthenticationFailure();
+            }
+        }).start();
     }
 }
